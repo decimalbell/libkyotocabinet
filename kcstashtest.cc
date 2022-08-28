c60a4bab298da4ad55d16e229bf09bc687f4078a
Junlong Xie <decimalbell@gmail.com>
2022-08-28 11:45:57 +0800

    feat: add kyotocabinet-1.2.77

--- kcstashtest.cc
+++ kcstashtest.cc
@@ -1976,7 +1976,7 @@ static int32_t proctran(int64_t rnum, int32_t thnum, int32_t itnum, int64_t bnum
               std::string key;
               if (cur->get_key(&key)) {
                 keys.push_back(key);
-                if (!cur->get_value(&key) && kc::BasicDB::Error::NOREC) {
+                if (!cur->get_value(&key) && db_->error() != kc::BasicDB::Error::NOREC) {
                   dberrprint(db_, __LINE__, "Cursor::get_value");
                   err_ = true;
                 }
